3aa5e74c55dcb22863e17bf62ae5b3c348e75769
Iam Nahid <iamnahid591998@gmail.com>
2025-06-17 17:10:57 +0600

    Done

---
+++ src/app/components/VoiceFlow.jsx
@@ -0,0 +1,16 @@
+
+import React from "react";
+
+const VoiceflowChatbot = () => {
+  return (
+    <iframe
+      src="https://creator.voiceflow.com/prototype/68496f4572b226e59cea9d24"
+      width="100%"
+      height="700px"
+      frameBorder="0"
+      title="Voiceflow Bot"
+    ></iframe>
+  );
+};
+
+export default VoiceflowChatbot;
